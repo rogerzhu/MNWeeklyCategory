9e557fb42e3e16cb512dab8839a8aa0c47b13b7d
rogerzhu <rogerzhu0710@126.com>
2019-11-11 15:24:29 +0800

    beautify the code to conform PEP-8

--- tools/Erase404.py
+++ tools/Erase404.py
@@ -4,16 +4,16 @@
 import requests
 from urllib import parse
 import os
-import click 
+import click
 from threading import Thread
-import time
 import fake_useragent
-from random import randint
+
 
 def get_header():
     location = os.getcwd() + '/agent.json'
     ua = fake_useragent.UserAgent(path=location)
     return ua.random
+
 
 def CountMDFileInAFolder(folder):
     file_names = os.listdir(folder)
@@ -24,17 +24,21 @@ def CountMDFileInAFolder(folder):
 
     return md_file_names
 
+
 @click.command()
-@click.option('--folder', default = '.', help='The folder that contains markdown files to be processed, default is current folder.')
+@click.option('--folder', default='.',
+              help='The folder that contains markdown files to be processed,\
+                   default is current folder.')
 def DeleteUnreachable(folder):
     if not os.path.exists('filtered'):
         os.mkdir('filtered')
-   
+
     threads = []
     md_file_names = CountMDFileInAFolder(folder)
 
     for x in range(len(md_file_names)):
-        thread = Thread(target=DeleteUnreachableCore,args=(md_file_names[x],))
+        thread = Thread(target=DeleteUnreachableCore,
+                        args=(md_file_names[x], ))
         threads.append(thread)
         thread.start()
 
@@ -42,12 +46,11 @@ def DeleteUnreachable(folder):
         thread.join()
 
 
-def DeleteUnreachableCore(file_name): 
-    lines = [] 
-    r = open(file_name,'r')
+def DeleteUnreachableCore(file_name):
+    r = open(file_name, 'r')
 
     for line in r:
-        #modified because the url format changed from serires #283
+        # modified because the url format changed from serires #283
         url = ''
         index_of_url = int(line.find('url='))
         index_of_aid = int(line.find('&aid'))
@@ -56,28 +59,29 @@ def DeleteUnreachableCore(file_name):
         else:
             url = line[line.find('url=')+4:-2]
 
-        if not 'http' in url:
+        if 'http' not in url:
             continue
 
         real_url = parse.unquote(parse.unquote(url))
         print(real_url)
-        if not 'toutiao.io' in real_url:
+        if 'toutiao.io' not in real_url:
             try:
-                headers={"User-Agent":get_header()}
-                r = requests.get(real_url,timeout=15,headers = headers)
+                headers = {"User-Agent": get_header()}
+                r = requests.get(real_url, timeout=15, headers=headers)
                 print(r.status_code)
                 if r.status_code == 200:
-                    with open('./filtered/'+ file_name,'a+') as w:
-                        w.write(line) 
-            except:
+                    with open('./filtered/' + file_name, 'a+') as w:
+                        w.write(line)
+            except Exception:
                 print('Not OK!')
                 pass
 
         else:
-            with open('./filtered/'+ file_name,'a+') as w:
+            with open('./filtered/' + file_name, 'a+') as w:
                 w.write(line)
 
     r.close()
+
 
 if __name__ == '__main__':
     processed_files = DeleteUnreachable()
